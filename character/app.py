3ea4e4f0d15b2882613fa6d740e30db7ebf12779
RuneSkovrupHansen <runeskovrup@gmail.com>
2022-07-25 21:29:50 +0200

    Update rest API

--- character/app.py
+++ character/app.py
@@ -1,6 +1,5 @@
-import json
 from flask import Flask, request
-from flask_restful import Resource, Api, abort
+from flask_restful import Resource, Api, abort, reqparse
 
 import character
 
@@ -40,6 +39,24 @@ def check_index(index):
         abort(404, message="Index {} does not exist".format(index))
 
 
+parser = reqparse.RequestParser()
+parser.add_argument("name", type=str)
+parser.add_argument("level", type=int)
+
+"""For a given resource it seems common to have two endpoints,
+one with and index, and one without, example:
+
+/resource/index
+/resource
+
+The endpoint with the index supports the methods GET, PUT 
+and DELETE which all use the index. The endpoint can
+support several methods, but if it makes sense include them
+all which supports getting one or more. The flask_restful
+refers to this endpoint as ResourceList.
+"""
+
+
 class Character(Resource):
 
     def get(self, index):
@@ -50,6 +67,23 @@ class Character(Resource):
         check_index(index)
         del characters_indexed[index]
         return '', 204
+
+    def put(self, index):
+        """curl command to call endpoint:
+        curl -X PUT http://127.0.0.1:5000/characters/0 -H 'Content-Type: application/json' -d '{"name":"rune", "level":100}'
+        """
+
+        args = parser.parse_args()
+        name = args['name']
+        level = args['level']
+
+        ret, char = character.Character(name, level).export_to_json()
+
+        if not ret:
+            abort(400, message="Invalid character data")
+
+        characters_indexed[index] = char
+        return characters_indexed[index], 201
 
 
 api.add_resource(Character, "/characters/<index>")
